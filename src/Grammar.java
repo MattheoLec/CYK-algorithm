2fb13c42c8ce9e7f3c931e1969c16c16b438fee6
Matthéo <mattheo9912@protonmail.com>
2022-10-19 12:42:19 +0200

    Grammar.java added

---
+++ src/Grammar.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class Grammar {
+    private String[][] TR; // Terminal rules
+    private String[][] NTR; // Non-terminal rules
+    private String [] convertedTR;
+    private List<List<Integer>> convertedNTR = new ArrayList<>();
+
+    public Grammar(String[][] TR, String[][] NTR) {
+        this.TR = TR;
+        this.NTR = NTR;
+        convertedTR = new String[NTR.length];
+        for (String[] rule : TR) {
+            if (rule.length != 2) {
+                throw new IllegalArgumentException("Terminal rules must have 2 elements (1 left-hand side and 1 right-hand side");
+            }
+            if (rule[1].length() != 1) {
+                throw new IllegalArgumentException("Right-hand side of Terminal rule must have 1 elements");
+            }
+        }
+        HashMap<Character, Integer> NTmap = new HashMap<>();
+        for (String[] rule : NTR) {
+            if (rule.length != 2) {
+                throw new IllegalArgumentException("Non terminal rules must have 2 elements (1 left-hand side and 1 right-hand side");
+            }
+            if (rule[1].length() != 2) {
+                throw new IllegalArgumentException("Right-hand side of Non terminal rules must have 2 elements");
+            }
+            if (!NTmap.containsKey(rule[0].charAt(0))) {
+                NTmap.put(rule[0].charAt(0), NTmap.size());
+            }
+            if (!NTmap.containsKey(rule[1].charAt(0))) {
+                NTmap.put(rule[1].charAt(0), NTmap.size());
+            }
+            if (!NTmap.containsKey(rule[1].charAt(1))) {
+                NTmap.put(rule[1].charAt(1), NTmap.size());
+            }
+        }
+        for (int i = 0; i < NTmap.size(); i++) {
+            convertedNTR.add(new ArrayList<>());
+        }
+        for (String[] strings : NTR) {
+            int nonTerminalIndex = NTmap.get(strings[0].charAt(0));
+            convertedNTR.get(nonTerminalIndex).add(NTmap.get(strings[1].charAt(0)));
+            convertedNTR.get(nonTerminalIndex).add(NTmap.get(strings[1].charAt(1)));
+        }
+        for (String[] strings : TR) {
+            int nonTerminalIndex = NTmap.get(strings[0].charAt(0));
+            convertedTR[nonTerminalIndex] = strings[1];
+        }
+    }
+
+    public void test() {
+        System.out.println(convertedNTR);
+        System.out.println(Arrays.toString(convertedTR));
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,21 @@ public class Main {
             {"F", "AD"}
         };
 
+
+        String[][] TR = new String[][]{
+                {"L", "("},
+                {"R", ")"},
+        };
+        String[][] NTR = new String[][]{
+                {"S", "SS"},
+                {"S", "LA"},
+                {"S", "LR"},
+                {"A", "SR"},
+        };
+
+        Grammar testNR = new Grammar(TR,NTR);
+        testNR.test();
+
         CYK test = new CYK("aaabbbcc", R);
         System.out.println("Inclusion of the word in the grammar: " + test.resolve());
         test.createTab();
